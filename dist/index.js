82885f7f7435357990a84367c9d8240ae243da94
himanshu kumar <writetohimanshukumar@gmail.com>
2025-10-02 13:39:25 +0530

    mongodb connection

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 const app = express();
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to mongodb');
+    }
+    catch (error) {
+        console.log('Error connecting to mongodb', error);
+    }
+})();
 app.get('/', (req, res) => {
     res.send('server...');
 });
